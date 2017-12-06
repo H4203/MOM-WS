a7b346342ab5edc01ba9222811038032a8ef5cb7
rgoubin <regis.goubin@insa-lyon.fr>
2017-12-06 08:36:16 +0100

    spotlight francais et epuration

--- RDFSearch/src/RDF/request.java
+++ RDFSearch/src/RDF/request.java
@@ -3,94 +3,71 @@ package RDF;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.vocabulary.*;
 
 public class request {
-	public void doRequest(String req) {
-		
-		String queryString=
-				"PREFIX p: <http://dbpedia.org/property/>"+
-				"PREFIX dbpedia: <http://dbpedia.org/resource/>"+
-				"PREFIX category: <http://dbpedia.org/resource/Category:>"+
-				"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"+
-				"PREFIX skos: <http://www.w3.org/2004/02/skos/core#>"+
-				"PREFIX geo: <http://www.georss.org/georss/>"+
+	public void doRequest(String queryString, String path) throws FileNotFoundException {
 
-				"SELECT DISTINCT ?m ?d "+
-				"WHERE {"+
-				"<http://dbpedia.org/resource/Berlin> ?m ?d}";
-		
 		Query query = QueryFactory.create(queryString);
-		
+
 		QueryExecution qexec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query);
 		FileOutputStream fop = null;
-		File file;
+		FileWriter file = null;
 		try {
-		    ResultSet results = qexec.execSelect();
-		    file = new File("./newfile");
+			ResultSet results = qexec.execSelect();
 			try {
-				fop = new FileOutputStream(file);
-			} catch (FileNotFoundException e) {
+				file = new FileWriter(new File("./newfile"), false);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			List<QuerySolution> list = ResultSetFormatter.toList(results);
+			for (int i = 0; i < list.size(); i++) {
+				try {
+					file.write("<" + list.get(i).get("x").toString() + "> <" + list.get(i).get("m").toString() + "> <"
+							+ list.get(i).get("d").toString() + "> ." + "\r\n");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+			try {
+				file.flush();
+			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		    /*for (; results.hasNext();) {
-		    	
-		    		System.out.println(results. + "  " + results.getResultVars().get(3));
-		    
-		    }*/
-		    ResultSetFormatter.out(fop, results, query); 
-		}
-		finally {
-		   qexec.close();
+
+		} finally {
+			qexec.close();
 		}
 
 	}
-	
+
 	public static void main(String[] args) {
-		// some definitions
-        /*String personURI    = "http://somewhere/JohnSmith";
-        String givenName    = "John";
-        String familyName   = "Smith";
-        String fullName     = givenName + " " + familyName;
-        // create an empty model
-        Model model = ModelFactory.createDefaultModel();
-
-        // create the resource
-        //   and add the properties cascading style
-        Resource johnSmith 
-          = model.createResource(personURI)
-                 .addProperty(VCARD.FN, fullName)
-                 .addProperty(VCARD.N, 
-                              model.createResource()
-                                   .addProperty(VCARD.Given, givenName)
-                                   .addProperty(VCARD.Family, familyName));
-        
-        // list the statements in the graph
-        StmtIterator iter = model.listStatements();
-        
-        // print out the predicate, subject and object of each statement
-        while (iter.hasNext()) {
-            Statement stmt      = iter.nextStatement();         // get next statement
-            Resource  subject   = stmt.getSubject();   // get the subject
-            Property  predicate = stmt.getPredicate(); // get the predicate
-            RDFNode   object    = stmt.getObject();    // get the object
-            
-            System.out.print(subject.toString());
-            System.out.print(" " + predicate.toString() + " ");
-            if (object instanceof Resource) {
-                System.out.print(object.toString());
-            } else {
-                // object is a literal
-                System.out.print(" \"" + object.toString() + "\"");
-            }
-            System.out.println(" .");
-        }*/
 		request r = new request();
-		r.doRequest("coucou");
-    }
-	}
+		String queryString = "PREFIX p: <http://dbpedia.org/property/>"
+				+ "PREFIX dbpedia: <http://dbpedia.org/resource/>"
+				+ "PREFIX category: <http://dbpedia.org/resource/Category:>"
+				+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
+				+ "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>" + "PREFIX geo: <http://www.georss.org/georss/>"
+				+
 
+				"SELECT DISTINCT ?x ?m ?d " + "WHERE {" + "?x ?m ?d "
+				+ "FILTER(?x = <http://dbpedia.org/resource/Berlin>)}";
+		try {
+			r.doRequest(queryString, "./newfile");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
--- RDFSearch/src/dbpedia/DBpediaSpotlightClient.java
+++ RDFSearch/src/dbpedia/DBpediaSpotlightClient.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 public class DBpediaSpotlightClient {
 
-	private final static String API_URL = "http://model.dbpedia-spotlight.org/en/annotate/?";
+	private final static String API_URL = "http://model.dbpedia-spotlight.org/fr/annotate/?";
 	private static final double CONFIDENCE = 0.3;
 	private static final int SUPPORT = 30;
 	private static HttpClient client = new HttpClient();
@@ -27,13 +27,14 @@ public class DBpediaSpotlightClient {
 		String spotlightResponse;
 		GetMethod getMethod = null;
 		try {
-			getMethod = new GetMethod(API_URL + "confidence=" + CONFIDENCE + "&support="
-					+ SUPPORT + "&text=" + URLEncoder.encode(text, "utf-8"));
+			getMethod = new GetMethod(API_URL + "confidence=" + CONFIDENCE + "&support=" + SUPPORT + "&text="
+					+ URLEncoder.encode(text, "utf-8"));
 			System.out.println(getMethod.getQueryString());
-			
+
 			getMethod.addRequestHeader(new Header("Accept", "application/json"));
+			getMethod.addRequestHeader(new Header("Language", "French"));
 			request(getMethod);
-			
+
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -46,46 +47,44 @@ public class DBpediaSpotlightClient {
 			System.out.println("Exception");
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
-	
+
 	public String request(HttpMethod method) {
 
-        String laresponse = null;
+		String laresponse = null;
 
-        // Provide custom retry handler is necessary
-        method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
-                new DefaultHttpMethodRetryHandler(3, false));
-        
+		// Provide custom retry handler is necessary
+		method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler(3, false));
 
-        try {
-            // Execute the method.
-            int statusCode = client.executeMethod(method);
+		try {
+			// Execute the method.
+			int statusCode = client.executeMethod(method);
 
-            
+			// Read the response body.
+			byte[] responseBody = method.getResponseBody(); // TODO Going to buffer response body of large or unknown
+															// size. Using getResponseBodyAsStream instead is
+															// recommended.
 
-            // Read the response body.
-            byte[] responseBody = method.getResponseBody(); //TODO Going to buffer response body of large or unknown size. Using getResponseBodyAsStream instead is recommended.
+			// Deal with the response.
+			// Use caution: ensure correct character encoding and is not binary data
+			laresponse = new String(responseBody);
 
-            // Deal with the response.
-            // Use caution: ensure correct character encoding and is not binary data
-            laresponse = new String(responseBody);
+		} catch (HttpException e) {
 
-        } catch (HttpException e) {
-            
-        } catch (IOException e) {
-            
-        } finally {
-            // Release the connection.
-            method.releaseConnection();
-        }
-        return laresponse;
+		} catch (IOException e) {
+
+		} finally {
+			// Release the connection.
+			method.releaseConnection();
+		}
+		return laresponse;
 	}
-	
-	 public static void main(String[] args) {
-		 String request = "Adolf Hitler Paris Mexique zubg zeiubvz vijb Youtube";
-		 DBpediaSpotlightClient d = new DBpediaSpotlightClient();
-		 d.extract(request);
-	 }
+
+	public static void main(String[] args) {
+		String request = "Fast and furious";
+		DBpediaSpotlightClient d = new DBpediaSpotlightClient();
+		d.extract(request);
+	}
 }
