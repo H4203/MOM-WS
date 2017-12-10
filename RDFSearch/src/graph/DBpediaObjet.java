10d0cbd626d00a03140e1694ccdfd665bca1c77d
shikshak <ft.mezidi@gmail.com>
2017-12-10 21:53:43 +0100

    hashmap<string,list<string>>

--- RDFSearch/src/graph/DBpediaObjet.java
+++ RDFSearch/src/graph/DBpediaObjet.java
@@ -1,59 +1,68 @@
-package graph;
+package dbpedia;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class DBpediaObjet {
-	
+
 	String URI;
-	HashMap<String, String> relationEtElement;
-	
+	HashMap<String, List<String>> relationEtElement;
+
 	public DBpediaObjet()
 	{
 		URI = "";
-		relationEtElement = new HashMap<>();
+		relationEtElement = new HashMap<String, List<String>>();
 	}
-	
+
 	public DBpediaObjet(String URI)
 	{
 		this.URI = URI;
-		relationEtElement = new HashMap<>();
+		relationEtElement = new HashMap<String, List<String>>();
 	}
-	
+
 	public void add(String relation, String element)
 	{
-		relationEtElement.put(relation, element);
+		List<String> listElement = new ArrayList<String>();
+		if(relationEtElement.get(relation) != null)
+		{
+			for( String ele : relationEtElement.get(relation) )
+			{
+				listElement.add(ele);
+			}
+		}
+		listElement.add(element);
+		relationEtElement.put(relation, listElement);
 	}
-	
-	//return the number of element added
-	//if negative, there was an error after adding |the return int| elements
+
 	public int addFromFile(String filePath)
 	{	
 		int nbAjoute = 0;
-		
+
 		try
 		{
 			BufferedReader br = new BufferedReader(new FileReader(filePath));
-		    String line = br.readLine();
-		    String uri, relation, element;
-		    
-		    String []str = line.split(" ");
-		    uri = str[0].substring(1, str[0].length()-2);
-		    this.setURI(uri);
-		    
-		    while (line != null) {
-		    	str = line.split(" ");
-		    	
-		    	relation = str[1].substring(1, str[1].length()-2);
-		    	element = str[2].substring(1, str[2].length()-2);
-		    	this.add(relation, element);
-		        line = br.readLine();
-		        nbAjoute++;
-		    }
-		    
-		    br.close();
+			String line = br.readLine();
+			String uri, relation, element;
+
+			String []str = line.split(" ");
+			uri = str[0].substring(1, str[0].length()-2);
+			this.setURI(uri);
+
+			while (line != null) {
+				str = line.split(" ");
+
+				relation = str[1].substring(1, str[1].length()-2);
+				element = str[2].substring(1, str[2].length()-2);
+				this.add(relation, element);
+				line = br.readLine();
+				nbAjoute++;
+			}
+
+			br.close();
 		}
 		catch(IOException e)
 		{
@@ -62,7 +71,7 @@ public class DBpediaObjet {
 		}
 		finally
 		{
-			
+
 		}
 		return nbAjoute;
 	}
@@ -75,15 +84,15 @@ public class DBpediaObjet {
 		URI = uRI;
 	}
 
-	public HashMap<String, String> getRelationEtElement() {
+	public HashMap<String, List<String>> getRelationEtElement() {
 		return relationEtElement;
 	}
-	/*
+/*
 	public static void main(String[] args)
 	{
 		DBpediaObjet ob = new DBpediaObjet();
 		ob.addFromFile("./newfile");
 		System.out.println(ob.getRelationEtElement());
 	}
-	*/
+*/
 }
